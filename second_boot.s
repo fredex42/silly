d17b59ccd39ef6dc8c457e0cb9e9f526b2b969dd
Andy Gallagher <andy.gallagher@theguardian.com>
2021-10-20 21:38:31 +0100

    theoretically working bootloader(!)

--- second_boot.s
+++ second_boot.s
@@ -1,8 +1,8 @@
 [BITS 16]
-[ORG 0x0000]	;set up sectors manually
+[ORG 0x7C00]	;boot loader lives here
 
 ;FAT format sector: https://networkencyclopedia.com/file-allocation-table-fat/#structure-of-fat-Volume
-jmp 0x7c0:_bootsect
+jmp _bootsect
 db 'ANDY_____'
 dw	0x0200	;512 bytes per logical sector
 db	0x04	;4 logical sectors per cluster - =>16-bit FAT
@@ -17,9 +17,9 @@ TIMES 24 db 0	;no extended parameter block
 _bootsect:
 ; set up code segment for bootloader
 ; set up stack
-mov ax, 0x07C0 ;4k stack space after the boot-loader
+mov ax, 0x1000 ;4k stack space after the boot-loader
 mov ss, ax
-mov sp, 4096
+mov sp, 0x2000
 
 ; set up data segment registers
 mov ax, 0x07E0	;start of 480k conventional memory
@@ -32,17 +32,6 @@ call PrintString
 
 ; locate and load in ANDY.SYS. It's assumed that this is the first entry in the FAT.
 
-;load first sector of FAT 0 into memory
-;mov ax, 0x07E0	;start of 480k conventional memory
-;mov es, ax
-;mov ds, ax	;used for reading in the data, below
-;mov bx,	0	;load in at the start of RAM
-;mov ax, 0x0201	;read in 1 sector
-;mov cx, 0x0004	;start at cylinder 0, sector 4 => 2k into disk
-;mov dx, 0x0080	; read from head 0 of HD 0. Drive number 0x80 => HD 0 (otherwise are floppies)
-;int 0x13
-;jc fat_load_err
-
 mov si, LoadingStart
 call PrintString
 
@@ -53,42 +42,13 @@ mov dx, 0x0080
 int 0x13
 jc no_extns
 
-;load the first FAT into memory
-;set up a disk address packet structure at 0x07E0:800. stos* stores at es:di and increments. int13 needs struct in ds:si
-cld		;make sure we are writing forwards
-mov di, 0x800
-mov al, 0x0F	;16 byte packet
-stosb
-xor al,al	;field always 0
-stosb
-mov ax, 0x01	;transfer 16 sectors
-stosw
-xor ax,ax	;write to 0x07E0:0000. Offset first.
-stosw
-mov ax, ds	;segment to write
-stosw
-mov ax, 0x0300	;we want to read one sector from offset of sector 4, but this is little-endian. Write 64 bits in 4 words.
-stosw
-xor ax,ax
-stosw
-stosw
-stosw		;now we are ready
-mov si, 0x800
-mov ah, 0x42
-mov dl, 0x80
-int 0x13
-jc fat_load_err
-
-;now get the first file from the first FAT
-mov si, 0x0004	;FAT is read in from 0x07E0:0000. We want to read from offset 4 which is the first file cluster.
-		; these go in ds:si
-next_cluster:
-lodsw		;ax is now the cluster number
+call FindFirstFileEntry
 
 loading_done:
 mov si, LoadingDone
 call PrintString
-jmp _hang
+; in theory, if we get to here ANDY.SYS is loaded at 0x7E0:0000.... here goes nothing!
+jmp 0x7E0:0000
 
 fat_load_err:
 cmp ah, 0x80	;0x80 is a timeout error and gets its own message
@@ -131,9 +91,71 @@ PrintString:
 	exit_function:
 	ret
 
+LoadDiskSectors:
+	;Loads disk sectors into memory, at 0x07E0(ds):0000. Overwrites this memory and clobbers registers.
+	;Arguments: bx = number of sectors to read in, cx = (16-bit) sector offset. Assume 1 sector = 512 bytes.
+	;set up a disk address packet structure at 0x07E0:800. stos* stores at es:di and increments. int13 needs struct in ds:si
+	cld		;make sure we are writing forwards
+	mov di, 0x800
+	mov al, 0x0F	;16 byte packet
+	stosb
+	xor al,al	;field always 0
+	stosb
+	mov ax, bx	;transfer 16 sectors
+	stosw
+	xor ax,ax	;write to 0x07E0:0000. Offset first.
+	stosw
+	mov ax, ds	;segment to write
+	stosw
+	mov ax, cx	;we want to read one sector from offset of sector 4, but this is little-endian. Write 48 bits in 4 words - LSW first
+	stosw
+	xor ax,ax
+	stosw
+	stosw
+	stosw		;now we are ready
+	mov si, 0x800
+	mov ah, 0x42
+	mov dl, 0x80
+	int 0x13
+	jc fat_load_err
+	ret
+
+FindFirstFileEntry:
+	;Expect directory entries to be from offset 0x8800 on the disk, which is sector 0x44
+	mov bx, 2
+	mov cx, 0x0044
+	call LoadDiskSectors
+	mov si, 0x0b
+	;now the first sector of the directory table is in memory at 0x07E0:0000. BUT the first entry could be an LFN.
+next_entry:
+	xor ax,ax
+	mov al, byte [si]
+	cmp al, 0x0f
+	add si, 0x20
+	jz next_entry
+	
+	;si now points to the attribute byte of the first entry that is not LFN. Subtract to get to the start of the record and check it's not null.
+	sub si, 0x0b
+	mov al, byte [si]
+	test al, al	;sets Z flag if ax is zero
+	jz fat_load_err
+
+	add si, 0x1a	;file size is 4 bytes at offset 1c of the record. Assume it's <64k for the time being.
+	mov ax, word [si+2]	;file size location
+	mov bx, 0x200
+	mul bx			;0x200 = 512, size of a sector in bytes
+	mov bx, ax		;MUL result always in AX, with high word in DX
+	mov ax, word [si]	;file start cluster. Assume that cluster size is 1800
+	mov bx, 0x0c
+	mul bx		;0x12=16 sectors per cluster
+	add ax, 0x44		;data area starts at 0x8800
+	mov cx, ax
+	call LoadDiskSectors
+	ret
+
 ;Data
 HelloString db ':D', 0x0a, 0x0d, 0
-CouldNotLoadErr db 'NoFAT', 0x0a, 0x0d, 0
+CouldNotLoadErr db 'DiskError', 0x0a, 0x0d, 0
 LoadingStart db 'Loading...', 0x0a, 0x0d, 0
 LoadingDone db 'Done', 0x0a, 0x0d, 0
 ;ErrCodes db '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
