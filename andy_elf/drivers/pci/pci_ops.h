31f90cf7628b1117fefb1d2c26aec09436dba768
Andy Gallagher <andy.gallagher@theguardian.com>
2024-04-17 20:15:28 +0100

    Partial categorisation of found devices during boot

--- andy_elf/drivers/pci/pci_ops.h
+++ andy_elf/drivers/pci/pci_ops.h
@@ -1,5 +1,6 @@
 #include <types.h>
 
+uint32_t pci_config_read_dword(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 void pci_preinit(void **pci_entrypoint);
 
--- andy_elf/drivers/pci/pci_scanner.c
+++ andy_elf/drivers/pci/pci_scanner.c
@@ -1,7 +1,105 @@
 #include "pci_scanner.h"
 #include "pci_ops.h"
+#include "pci_categorisation.h"
 
 //Mostly taken from https://wiki.osdev.org/PCI#Enumerating_PCI_Buses !
+const char* pci_description_string(uint8_t pci_class, uint8_t subclass) {
+    switch(pci_class) {
+        case PCI_CC_UNCLASSIFIED:
+            switch(subclass) {
+                case PCI_SUBC_NON_VGA_UNCLASSIFIED:
+                    return "Unclassified non-VGA-compatible device";
+                case PCI_SUBC_VGA_COMPATIBLE_UNCLASSIFIED:
+                    return "Unclassified VGA-compatible device";
+                default:
+                    return "Unknown unclassified device";
+            }
+            break;
+        case PCI_CC_MASS_STORAGE:
+            switch(subclass) {
+                case PCI_SUBC_MASS_SCSI:
+                    return "SCSI Mass Storage Controller";
+                case PCI_SUBC_MASS_IDE:
+                    return "IDE Mass Storage Controller";
+                case PCI_SUBC_MASS_FLOPPY:
+                    return "Floppy drive controller";
+                case PCI_SUBC_MASS_IPI:
+                    return "IPI Mass storage Controller";
+                case PCI_SUBC_MASS_RAID:
+                    return "RAID Controller";
+                case PCI_SUBC_MASS_ATA:
+                    return "ATA Mass Storage Controller";
+                case PCI_SUBC_MASS_SAS:
+                    return "Serial-attached SCSI Controller";
+                case PCI_SUBC_MASS_NVM:
+                    return "Non-volatile memory Mass Storage";
+                default:
+                    return "Unknown Mass Storage controller";
+            }
+            break;
+        case PCI_CC_NETWORK:
+            switch(subclass) {
+                case PCI_SUBC_NET_ETHER:
+                    return "Ethernet Controller";
+                case PCI_SUBC_NET_TOKEN:
+                    return "Token-ring Controller";
+                case PCI_SUBC_NET_FDDI:
+                    return "FDDI Fibre Controller";
+                case PCI_SUBC_NET_ATM:
+                    return "ATM Network Controller";
+                case PCI_SUBC_NET_ISDN:
+                    return "ISDN Controller";
+                case PCI_SUBC_NET_WORLDFIP:
+                    return "WorldFIP Controller";
+                case PCI_SUBC_NET_INFINIBAND:
+                    return "Infiniband Network Controller";
+                case PCI_SUBC_NET_FABRIC:
+                    return "Fabric Controller";
+                default:
+                    return "Unknown Network Controller";
+            }
+        case PCI_CC_DISPLAY:
+            switch(subclass) {
+                case PCI_SUBC_DISPLAY_VGA:
+                    return "VGA-Compatible Display Controller";
+                case PCI_SUBC_DISPLAY_XGA:
+                    return "XGA-Compatible Display Controller";
+                case PCI_SUBC_DISPLAY_3DCTRL:
+                    return "Non-VGA Compatible 3d Display Controller";
+                default:
+                    return "Unknown Display Controller";
+            }
+        case PCI_CC_BRIDGE:
+            switch (subclass) {
+            case PCI_SUBC_BRIDGE_HOST:
+                return "PCI Host Bridge";
+            case PCI_SUBC_BRIDGE_ISA:
+                return "PCI-ISA Bridge";
+            case PCI_SUBC_BRIDGE_EISA:
+                return "PCI-Extended ISA Bridge";
+            case PCI_SUBC_BRIDGE_MCA:
+                return "PCI-MCA Bridge";
+            case PCI_SUBC_BRIDGE_PCI:
+                return "PCI-PCI Bridge";
+            case PCI_SUBC_BRIDGE_PCMCIA:
+                return "PCI-PCMCIA Bridge";
+            case PCI_SUBC_BRIDGE_NUBUS:
+                return "PCI-NuBus Bridge";
+            case PCI_SUBC_BRIDGE_CARDBUS:
+                return "PCI-CardBus Bridge";
+            case PCI_SUBC_BRIDGE_RACEWAY:
+                return "Raceway Bridge";
+            case PCI_SUBC_BRIDGE_PCI2:
+                return "PCI-PCI Bridge (#2)";
+            case PCI_SUBC_BRIDGE_INFINIBAND:
+                return "PCI-Infiniband Bridge";
+            default:
+                return "Unknown PCI Bridge";
+            }
+        default:
+            return "Not recognised yet!";
+    }
+}
 
 /*
  * Checks to see if there is a device at the given address on the given bus
@@ -26,7 +124,7 @@ void pci_scanner_check_device(uint8_t bus, uint8_t device) {
         //This is a multi-function device, check remaining functions (up to 8)
         for(function = 1; function < 8; function++) {
             uint16_t subvendor_id = pci_config_read_word(bus, device, function, 0);
-            if(vendor_id != 0xFFFF) {
+            if(subvendor_id != 0xFFFF) {
                 pci_scanner_check_function(bus, device, function);
             }
         }
@@ -42,6 +140,7 @@ void pci_scanner_check_function(uint8_t bus, uint8_t device, uint8_t function) {
     uint8_t base_class = (uint8_t)(class_values >> 8);
     uint8_t sub_class  = (uint8_t)(class_values & 0xFF);
     kprintf("    DEBUG 0x%x:0x%x:0x%x is class 0x%x:0x%x\r\n", (uint32_t)bus, (uint32_t)device, (uint32_t)function, (uint32_t)base_class, (uint32_t)sub_class);
+    kprintf("    DEBUG Found '%s'\r\n", pci_description_string(base_class, sub_class));
     if(base_class==0x06 && sub_class==0x04) {
         uint16_t bus_values = pci_config_read_word(bus, device, function, 0x18);    //Primary & secondary bus values
         uint8_t secondary_bus = (uint8_t)(bus_values >> 8);
