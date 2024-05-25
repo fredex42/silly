95fb5b31297462a06a1f6681b4f41cc65fef93b0
Andy Gallagher <andy.gallagher@theguardian.com>
2024-05-25 16:14:04 +0100

    work on keyboard decoding

---
+++ andy_elf/drivers/ps2_controller/keymap.c
@@ -0,0 +1,49 @@
+#include <types.h>
+#include "keymap.h"
+#include "builtin_keymap.h"
+#include <panic.h>
+
+char *active_keymap_ptr = builtin_keymap;
+
+void set_active_keymap(char *keymap, size_t len)
+{
+    if(!keymap) {
+        kputs("ERROR set_active_keymap called with invalid keymap value\r\n");
+        return;
+    }
+
+    uint16_t keymap_len = *(uint16_t *) keymap; //first 2 bytes are the map length
+    if(keymap_len != (uint16_t) len) {
+        kputs("ERROR keymap provided to set_active_keymap had inconsistent length\r\n");
+        kprintf("Keymap said 0x%x, provided length was 0x%x\r\n", keymap_len, len);
+        return;
+    }
+
+    active_keymap_ptr = keymap;
+}
+
+void set_builtin_keymap()
+{
+    kputs("INFO Resetting to built-in keymap\r\n");
+    active_keymap_ptr = builtin_keymap;
+}
+
+/**
+ * Returns the ASCII code (if available) for the given scancode using the given shiftmask (state of CTRL, ALT, SHIFT etc. keys)
+ * Conversion is carried out by the currently active keymap
+*/
+char lookup_scancode(char scancode, uint8_t shiftmask)
+{
+    if(!active_keymap_ptr) {
+        k_panic("lookup_scancode called without an active keymap!\r\n");
+    }
+
+    uint16_t keymap_len = *(uint16_t *) active_keymap_ptr;  //first 2 bytes are the map length
+    size_t keymap_pos = (scancode * 2); //code 1 is at position 3+4 (0-based byte 2 + 3), there is no code 0
+    if(shiftmask & SHIFTMASK_SHIFT) keymap_pos+=1;  //shifted equivalent is at +1
+    if(keymap_pos>keymap_len) {
+        kprintf("ERROR scancode 0x%x out of range, limit was 0x%x\r\n", (uint32_t)scancode, (uint32_t)keymap_len);
+        return 0;
+    }
+    return active_keymap_ptr[keymap_pos];
+}
