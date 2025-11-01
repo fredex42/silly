a6985b16f34c356d2fd449607c4f1ccb3ed071c2
Andy Gallagher <fredex42@gmail.com>
2025-11-01 12:08:50 +0100

    compilation fixes, prototypes and such

--- andy_elf/volmgr/volmgr_internal.h
+++ andy_elf/volmgr/volmgr_internal.h
@@ -4,9 +4,23 @@
 #include <volmgr.h>
 #include <fs.h>
 
+// Forward declarations for internal use
+struct VolMgr_Disk;
+struct VolMgr_Volume;
+
+// Function prototypes for internal and external linkage
+void kputs(const char *fmt, ...);
+void kprintf(const char *fmt, ...);
+void volmgr_disk_ref(struct VolMgr_Disk *disk);
+void volmgr_disk_unref(struct VolMgr_Disk *disk);
+uint8_t volmgr_initialise_disk(struct VolMgr_Disk *disk);
+uint8_t volmgr_isa_disk_number(struct VolMgr_Disk *disk);
+void vol_mounted_cb(uint8_t status, void *fs_ptr, void *extradata);
+
 struct VolMgr_Disk {
     struct VolMgr_Disk *next;
     uint32_t disk_id;
+    uint32_t refcount;
     enum disk_type type;
     uint32_t base_addr;
     uint32_t flags;
@@ -19,9 +33,11 @@ struct VolMgr_Disk {
 
 struct VolMgr_Volume {
     struct VolMgr_Volume *next;
+    uint32_t refcount;
     uint32_t start_sector;
     uint32_t sector_count;
     //More fields as needed
+    struct VolMgr_Disk *disk;
     enum PartitionType part_type;
     void *fs_ptr; //Pointer to filesystem instance - type is determined by PartitionType
 };
@@ -37,4 +53,14 @@ struct volmgr_internal_mount_data {
     struct VolMgr_Volume *volume;
     void (*callback)(uint8_t status, void *fs_ptr, void *extradata);
 };
+
+/**
+ * Internal function to start a read operation using the correct underlying storage driver.
+ * You should not call this directly, rather call vol_start_read which will correctly set lba_address
+ * with partition offsets
+ */
+int8_t volmgr_disk_start_read(struct VolMgr_Disk *disk, uint64_t lba_address, uint16_t sector_count, void *buffer, void *extradata, void (*callback)(uint8_t status, void *buffer, void *extradata));
+int8_t volmgr_disk_start_write(struct VolMgr_Disk *disk, uint64_t lba_address, uint16_t sector_count, void *buffer, void *extradata, void (*callback)(uint8_t status, void *buffer, void *extradata));
+
+
 #endif
