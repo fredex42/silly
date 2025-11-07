ec0596e1d342b0384da5d9da149be2946eb66999
Andy Gallagher <andy.gallagher@theguardian.com>
2025-11-07 14:25:58 +0000

    fix shell.app compilation

--- andy_elf/process/elfloader.c
+++ andy_elf/process/elfloader.c
@@ -255,7 +255,7 @@ void _elf_loaded_file_header(VFatOpenFile *fp, uint8_t status, size_t bytes_read
     return;
   }
   //check the OS ABI (we imply Linux support for compatibility with existing tools)
-  if(header->ident_os_abi != ELF_ABI_SILLY && header->ident_os_abi != ELF_ABI_LINUX) {
+  if(header->ident_os_abi != ELF_ABI_SILLY && header->ident_os_abi != ELF_ABI_LINUX && header->ident_os_abi!=0) {
     kprintf("ERROR: Unsupported ELF OS ABI %d\r\n", header->ident_os_abi);
     t->callback(E_UNSUPPORTED_ELF, t->parsed_data, t->extradata);
     free(t);
