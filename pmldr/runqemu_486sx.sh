a0a134e458be49f0fbbfdfe2074fd16f96390bf6
Andy Gallagher <fredex42@gmail.com>
2025-11-11 16:50:47 +0000

    script to emulate old 486 boot

---
+++ pmldr/runqemu_486sx.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+qemu-system-i386 -hda test.fat -m 4 -M isapc -cpu 486 -serial stdio \
+    -device sb16 -vga cirrus -boot c -rtc base=localtime 
+    #-device isa-fdc,drive=floppy0 -drive id=floppy0,index=1,format=raw,media=disk  \
