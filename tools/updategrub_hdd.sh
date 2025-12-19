ce849c9e793a8938527e4f509ff9de9785cf2297
Andy Gallagher <andy.gallagher@theguardian.com>
2025-12-19 10:31:02 +0000

    quicker update script

---
+++ tools/updategrub_hdd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+if [ ! -f disk.img ]; then
+    echo "disk.img not found!"
+    exit 1
+fi
+
+if [ ! -f ../andy_elf/test.elf ]; then
+    echo "Kernel ELF not found!"
+    exit 1
+fi
+
+if [ ! -f ../shell.app/shell.app ]; then
+    echo "Shell application not found!"
+    exit 1
+fi
+
+sudo kpartx -av disk.img
+sudo mount /dev/mapper/loop0p1 /mnt
+sudo cp ../andy_elf/test.elf /mnt/boot/mykernel.elf
+sudo cp ../shell.app/shell.app /mnt
+sudo umount /mnt
+sudo kpartx -dv disk.img
